d416ddc9f7cc99d0e3d1fcbef509de3ed6d1c648
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2017-03-27 20:52:14 +0300

    Remove unused custom types

--- src/backends/coreconfig_cpp11_clang.hpp
+++ src/backends/coreconfig_cpp11_clang.hpp
@@ -32,18 +32,6 @@
 #include <stdint.h>
 #include <wchar.h>
 
-using uint8 = unsigned char;
-using int8 = char;
-
-using uint16 = unsigned short;
-using int16 = short;
-
-using uint32 = unsigned int;
-using int32 = int;
-
-using uint64 = unsigned long long;
-using int64 = long long;
-
 using byte = unsigned char;
 
 #undef SIZEOF_WCHAR_T
@@ -54,30 +42,6 @@ using byte = unsigned char;
 #else
 #define SIZEOF_WCHAR_T -1  // Unknown GCC platform
 #endif
-
-#undef INT8_MAX
-#define INT8_MAX SCHAR_MAX
-#undef INT8_MIN
-#define INT8_MIN SCHAR_MIN
-
-#undef INT16_MAX
-#define INT16_MAX SHRT_MAX
-#undef INT16_MIN
-#define INT16_MIN SHRT_MIN
-
-#undef INT32_MAX
-#define INT32_MAX INT_MAX
-#undef INT32_MIN
-#define INT32_MIN INT_MIN
-
-#undef UINT8_MAX
-#define UINT8_MAX UCHAR_MAX
-
-#undef UINT16_MAX
-#define UINT16_MAX USHRT_MAX
-
-#undef UINT32_MAX
-#define UINT32_MAX UINT_MAX
 
 #if (TARGET_OS == OS_LINUX)
 
